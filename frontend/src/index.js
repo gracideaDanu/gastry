11b8d8375e70dda2224a23229c975a94299d0d95
fadel <mk322@hdm-stuttgart.de>
2020-06-03 23:48:22 +0200

    created persistStore to persist user data in redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from "./Routes";
-import {Provider} from "react-redux";
-import store from "./redux/store";
-import {BrowserRouter} from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <Routes class="routes"/>
-            </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <Routes class="routes"/>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>, document.querySelector('#root'))
-
