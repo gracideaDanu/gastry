ae606fcd8345592070cec6d1b249583f5321f1a8
krtee <minh.vu.nguyen@hotmail.de>
2020-06-07 22:14:06 +0200

    merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from "./Routes";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from 'redux-persist/integration/react'
+import {Provider} from "react-redux";
+import {store, persistor} from "./redux/store";
+import {BrowserRouter} from "react-router-dom";
+import {PersistGate} from 'redux-persist/integration/react'
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import './customs.scss';
 
@@ -17,14 +15,11 @@ import './customs.scss';
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <Container className="justify-content-center" id={"rootcontainer"}>
-                        <Routes class="routes"/>
-                </Container>
-            </BrowserRouter>
-        <PersistGate loading={null} persistor={persistor}>
+            <PersistGate loading={null} persistor={persistor}>
                 <BrowserRouter>
-                    <Routes class="routes"/>
+                    <Container className="justify-content-center" id={"rootcontainer"}>
+                        <Routes class="routes"/>
+                    </Container>
                 </BrowserRouter>
             </PersistGate>
         </Provider>
