9bc9072c87f13ca5e8a55067e35d9ad8ac7769c1
krtee <minh.vu.nguyen@hotmail.de>
2020-06-09 23:14:40 +0200

    minor bug fixes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -56,8 +56,6 @@ class Routes extends Component {
 
                     { /* Routes requiring login */}
                     {privateRoutes}
-                    <Route exact path={"/home"} component={HomeCustomer}></Route>
-                    <Route exact path={"/catalogSupplier"} component={Catalog}/>
                     <Route exact path={"/login"} component={Login}/>
                     <Route exact path={"/register"} component={Signup}/>
 
--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -2,8 +2,6 @@ import React, {} from 'react'
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "./nav.scss";
-import {LinkContainer} from "react-router-bootstrap";
-import NavItem from "react-bootstrap/NavItem";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
