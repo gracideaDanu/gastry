079f748dc495b309ba1ab2693cfe53b02cf01e4a
fadel <mk322@hdm-stuttgart.de>
2020-06-27 21:08:25 +0200

    added search to CatalogCustomer

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -4,14 +4,19 @@ import FormControl from "react-bootstrap/FormControl";
 import "./Search.css";
 
 class Search extends Component {
-
     onChange = (e) => {
-        this.props.onChange(e.target.value)
-    }
+        this.props.onChange(e.target.value);
+    };
 
     render() {
         return (
-            <FormControl type="text" placeholder="Search" className="mr-sm-2 searchInput" onChange={this.onChange} value={this.props.value}/>
+            <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2 searchInput"
+                onChange={this.onChange}
+                value={this.props.value}
+            />
         );
     }
 }
--- frontend/src/containers/customer/catalogCustomer/CatalogCustomer.js
+++ frontend/src/containers/customer/catalogCustomer/CatalogCustomer.js
@@ -3,103 +3,105 @@ import { connect } from "react-redux";
 import * as actions from "../../../redux/actions";
 import SupplierCatList from "../../../components/list/CatalogCustomerRow.js/CatalogCustomerRow";
 import CustomerLaylout from "../CustomerLayout";
-import {addItemToBasket} from "../../../redux/actions";
+import Search from "../../../components/search/Search";
+import { addItemToBasket } from "../../../redux/actions";
 import pt from "moment/locale/pt";
 
 let basketArray = [];
-let supplierName, supplierId
+let supplierName, supplierId;
 class CatalogCustomer extends Component {
     state = {
         basket: [],
-        catalog: []
+        catalog: [],
+        filteredList: [],
+        searchInputValue: "",
+    };
 
-    }
     componentDidMount() {
         supplierId = this.props.location.state.supplierId;
         supplierName = this.props.location.state.supplierName;
         this.props.fetchSupplierCatalog(supplierId);
-         if(this.props.basket !== null){
-             basketArray = [...this.props.basket];
-            let basketSingle = basketArray.find(basket => basket.supplierId === supplierId);
+        if (this.props.basket !== null) {
+            basketArray = [...this.props.basket];
+            let basketSingle = basketArray.find(
+                (basket) => basket.supplierId === supplierId
+            );
             let basketItems = [];
-            basketSingle ? basketItems = [...basketSingle.basketItems] : basketItems = []
+            basketSingle
+                ? (basketItems = [...basketSingle.basketItems])
+                : (basketItems = []);
             console.log(basketItems);
-            if(basketItems !== null){
+            if (basketItems !== null) {
                 this.setState({
                     ...this.state,
-                    basket: basketItems
-            })
+                    basket: basketItems,
+                });
             }
-
-
         }
-
     }
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if(prevProps.catalog !== this.props.catalog){
+        if (prevProps.catalog !== this.props.catalog) {
             this.setState({
                 ...this.state,
-                catalog: this.props.catalog
-            })
+                catalog: this.props.catalog,
+            });
         }
         this.findItemAmount("5eea44202681241ad4fd9c42");
-
-
     }
     addItemToBasketHandler = (event, itemId, option, amount) => {
         event.preventDefault();
         const { supplierId } = this.props.location.state;
 
         const catalog = [...this.state.catalog];
-        let item = catalog.find(catItem => catItem._id === itemId );
+        let item = catalog.find((catItem) => catItem._id === itemId);
         const basket = [...this.state.basket];
-        let incrementItem = basket.find(element => item._id === element._id)
+        let incrementItem = basket.find((element) => item._id === element._id);
         if (incrementItem) {
-            const index = basket.findIndex(element => element === incrementItem);
+            const index = basket.findIndex(
+                (element) => element === incrementItem
+            );
             let amount = incrementItem.amount;
             switch (option) {
-                case 1: amount += 1;
-                break;
-                case 2: amount -= 1;
-                console.log(amount)
-                break;
-
-
+                case 1:
+                    amount += 1;
+                    break;
+                case 2:
+                    amount -= 1;
+                    console.log(amount);
+                    break;
             }
-            amount < 0 ? amount = 0 : amount = amount
-            amount > 99 ? amount = 99 : amount = amount
+            amount < 0 ? (amount = 0) : (amount = amount);
+            amount > 99 ? (amount = 99) : (amount = amount);
             incrementItem = {
                 ...incrementItem,
-                amount: amount
+                amount: amount,
             };
-            amount === 0 ? basket.splice(index,1) : basket[index] = incrementItem
-
-
-        }
-        else {
-            switch(option){
-                case 2: return; break;
-                default: item = {
-                    ...item,
-                    amount: 1
-                };
-                    basket.push(item)
+            amount === 0
+                ? basket.splice(index, 1)
+                : (basket[index] = incrementItem);
+        } else {
+            switch (option) {
+                case 2:
+                    return;
+                    break;
+                default:
+                    item = {
+                        ...item,
+                        amount: 1,
+                    };
+                    basket.push(item);
             }
             console.log("New item");
-
         }
         this.setState({
             ...this.state,
-            basket: basket
-        })
+            basket: basket,
+        });
         this.props.addItemToBasket({
-                basket: basket,
-            supplierId: supplierId
-        })
-        console.log("HI PROPS")
-
-
-
+            basket: basket,
+            supplierId: supplierId,
+        });
+        console.log("HI PROPS");
     };
     findItemAmount = (itemId) => {
         let amount;
@@ -114,35 +116,65 @@ class CatalogCustomer extends Component {
         return amount;
         return 0*/
         let basket = [...this.state.basket];
-        let item = basket.find(inc => inc._id === itemId);
-        item ? amount = item.amount : amount = 0;
+        let item = basket.find((inc) => inc._id === itemId);
+        item ? (amount = item.amount) : (amount = 0);
         return amount;
+    };
 
+    handleInputChange = (searchInputValue) => {
+        this.setState({ searchInputValue });
+        this.handleSearch();
+    };
+
+    handleSearch = () => {
+        if (this.state.searchInputValue !== "") {
+            const filteredList = this.props.catalog.filter((row) => {
+                const nameToLowerCase = row.name.toLowerCase();
+                const filter = this.state.searchInputValue.toLowerCase();
+                return nameToLowerCase.includes(filter);
+            });
+            this.setState({ filteredList });
+        }
     };
 
     renderCatalog = () => {
-        if (this.props.catalog) {
-            if (this.props.catalog.length === 0) {
-                return <div>No Items Found</div>;
-            }
-            const catArray = this.props.catalog.map((item, index) => (
-                <SupplierCatList
-                    amount={this.findItemAmount(item._id)}
-                    add={(e) => this.addItemToBasketHandler(e,item._id,1)}
-                    subtract={(e) => this.addItemToBasketHandler(e,item._id,2)}
-                    key={item._id}
-                    index={index}
-                    item={item}
-                ></SupplierCatList>
-            ));
-            return <div>{catArray}</div>;
+        const { catalog } = this.props;
+        const { searchInputValue, filteredList } = this.state;
+        const renderedList = searchInputValue.length > 0 ? filteredList : catalog;
+
+        if (!catalog) return <div>loading</div>;
+
+        if (catalog.length === 0) {
+            return <div>No Items Found</div>;
         }
-        return <div>loading</div>;
+        
+        return renderedList.map((item, index) => (
+            <SupplierCatList
+                amount={this.findItemAmount(item._id)}
+                add={(e) => this.addItemToBasketHandler(e, item._id, 1)}
+                subtract={(e) => this.addItemToBasketHandler(e, item._id, 2)}
+                key={item._id}
+                index={index}
+                item={item}
+            ></SupplierCatList>
+        ));
     };
 
     render() {
         return (
-            <CustomerLaylout title="Catalog" showBasket='true'  basketState={{supplierName: supplierName, supplierId: supplierId, basket:this.state.basket}} >
+            <CustomerLaylout
+                title="Catalog"
+                showBasket="true"
+                basketState={{
+                    supplierName: supplierName,
+                    supplierId: supplierId,
+                    basket: this.state.basket,
+                }}
+            >
+                <Search
+                    onChange={this.handleInputChange}
+                    value={this.state.searchInputValue}
+                />
                 {this.renderCatalog()}
             </CustomerLaylout>
         );
@@ -152,7 +184,7 @@ class CatalogCustomer extends Component {
 const mapsStateToProps = (state) => {
     return {
         catalog: state.customerCatalog.catalog,
-        basket: state.basket.baskets
+        basket: state.basket.baskets,
     };
 };
 
@@ -160,7 +192,8 @@ const mapDispatchToProps = (dispatch) => {
     return {
         fetchSupplierCatalog: (payload) =>
             dispatch(actions.fetchSupplierCatalog(payload)),
-        addItemToBasket: (payload) => dispatch(actions.addItemToBasket(payload))
+        addItemToBasket: (payload) =>
+            dispatch(actions.addItemToBasket(payload)),
     };
 };
 
