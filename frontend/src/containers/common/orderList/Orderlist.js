f116a60e4f505aba05cb4c9ff72cf13504440a82
fadel <mk322@hdm-stuttgart.de>
2020-07-13 23:13:04 +0200

    adjusted the navbar. changed layout based on userType

--- frontend/src/containers/common/orderList/Orderlist.js
+++ frontend/src/containers/common/orderList/Orderlist.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import * as actions from "../../../redux/actions";
 import { connect } from "react-redux";
-import CustomerLayout from "../CustomerLayout";
+import CustomerLayout from "../../customer/CustomerLayout";
+import SupplierLayout from "../../supplier/SupplierLayout";
 import OrderListItem from "../../../components/orders/OrderListItem";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
 class Orderlist extends Component {
     componentDidMount() {
+        console.log(this.props.userType)
         let payload = {
             token: this.props.token,
         };
@@ -15,6 +17,7 @@ class Orderlist extends Component {
     }
 
     render() {
+        
         const orders = this.props.orders.map((item) => (
             <Link
                 to={{
@@ -36,7 +39,11 @@ class Orderlist extends Component {
                 )}
             </Link>
         ));
-        return (
+        return this.props.userType === "supplier" ? (
+            <SupplierLayout title="Orders" description="" location={"orders"}>
+                <Container fluid>{orders}</Container>
+            </SupplierLayout>
+        ) : (
             <CustomerLayout title="Orders" description="" location={"orders"}>
                 <Container fluid>{orders}</Container>
             </CustomerLayout>
