4cbb396c954e4401b21f5170adb94cb92b0803a3
Ferrari Domenico <df050@hdm-stuttgart.de>
2020-07-17 00:47:31 +0200

    not changing amount by typing was intentional, but now its implemented.

--- frontend/src/components/counter/Counter.js
+++ frontend/src/components/counter/Counter.js
@@ -9,7 +9,7 @@ const Counter = (props) => {
             <button className="counterBtn" onClick={props.countDown}>
                 <img src={add} width="25" height="25" alt={"increase"}/>
             </button>
-            <input className="counterInput text-center" value={props.amount} type="text" size="1" onChange={props.onChange} ></input>
+            <input type="number" maxLength="2" minLength="1" defaultValue="1" min="0" max="99" className="counterInput text-center" value={props.amount} type="text" size="1" onChange={(e) => props.onChange(e,props.itemId)} ></input>
             <button className="counterBtn" onClick={props.countUp}>
                 <img src={add} width="25" height="25" alt={"increase"}/>
             </button>
