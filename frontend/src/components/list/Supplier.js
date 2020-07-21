fb998b180bf1ce6ae64198b7ceab18d833fb8103
fadel <mk322@hdm-stuttgart.de>
2020-07-22 01:24:39 +0200

    styling: suppliers list

--- frontend/src/components/list/Supplier.js
+++ frontend/src/components/list/Supplier.js
@@ -1,29 +1,29 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Card from "react-bootstrap/Card";
-import "./Supplier.scss"
 import goto from "../../assets/icons/next.svg";
+import logo from "../../assets/icons/logo.svg";
+
+import "./Supplier.scss";
 
 const Supplier = (props) => {
     return (
-        <Card className="supplier-card">
-            <Card.Body>
-                <Row className="align-items-center">
-                    <Col xs={2}>
-                        <img className={"supplierlogo"} src={props.pic} width="30" height="30" alt={props.name}/>
-                    </Col>
-                    <Col xs={8}>
-                        <p>{props.name}</p>
-
-                        <p>{props.address}</p>
-                    </Col>
-                    <Col xs={2}>
-                        <img className={"forward"} src={goto} width="30" height="30" alt={"back"}/>
-                    </Col>
-                </Row>
-            </Card.Body>
-        </Card>
+        <div className="supplier-container">
+            <img
+                className={"supplier-logo"}
+                src={logo}
+                alt={props.name}
+            />
+            <div className="supplier-info">
+                <p>{props.name}</p>
+                <p>{props.address}</p>
+            </div>
+            <img
+                className={"forward-icon"}
+                src={goto}
+                width="40"
+                height="40"
+                alt={"back"}
+            />
+        </div>
     );
 };
 
